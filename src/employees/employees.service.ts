0a3ef29cf7f4ba56da32e15e9e2da40829b7564c
TeodorTod <teodor.at.tod@gmail.com>
2024-10-16 11:48:58 +0300

    final

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -44,6 +44,10 @@ export class EmployeesService {
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} employee`;
+    return this.databaseService.employee.delete({
+      where: {
+        id
+      }
+    })
   }
 }
